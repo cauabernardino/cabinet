699db12f418cef7fb773d76255225da78d959ac6
Cauã Bernardino <cbernardino@live.com>
2022-01-30 19:18:02 +0000

    Initial setup config.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='cabinet',
+    version='0.0.1',
+    description='A simple wrapper tool to run scripts easily from CLI.',
+    long_description=readme,
+    author='Cauã Bernardino',
+    author_email='',
+    url='https://github.com/cauabernardino/cabinet',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
